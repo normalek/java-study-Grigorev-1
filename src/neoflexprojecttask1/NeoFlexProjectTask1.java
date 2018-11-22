6164be18167c563b27373df1c530cc49fc945088
normalek <filmacker@gmail.com>
2018-11-22 22:39:22 +0300

    Comments according the project

--- src/neoflexprojecttask1/NeoFlexProjectTask1.java
+++ src/neoflexprojecttask1/NeoFlexProjectTask1.java
@@ -3,44 +3,43 @@ package neoflexprojecttask1;
 import java.util.*;
 
 public class NeoFlexProjectTask1 {
-    static Hashtable<String,Integer> countWords;
+    static HashMap<String,Integer> countWords;// лучше Hashtable не использовать если нет использования многопоточности, заменить на HashMap
 
     public static void main(String[] args) {
         searchWord("Hello Dima, how are you? Hello, I'am Dima dima", "Dima Hello", true);
-        Iterator itr = countWords.keySet().iterator();
         while(itr.hasNext()) {
-            String word = (String)itr.next();
-            System.out.println("Слово: " + word + " встречается " + 
-                    (int) countWords.get(word) + " раз");
+            String word = (String)itr.next(); //можно без итератора сделать вот так - for (String word : countWords.keySet()) {
+            System.out.println("Слово: " + word + " встречается " +
+                    (int) countWords.get(word) + " раз"); // здесь не нужен каст к int
         }
     }
     
-    public static Hashtable<String,Integer> searchWord(String text, String word, boolean ignorCase) {
-        countWords = new Hashtable<String,Integer>();
+    public static HashMap<String,Integer> searchWord(String text, String word, boolean ignorCase) { //метод желательно сделать private, т.к. больше нигде не предполагается его использовать
+        countWords = new HashMap<String,Integer>(); //обычно инициализация неразрывна с объявлением ссылки + т.к. используется Java 8, то лучше сделать diamond annotations
         String[] wordsFromTheText = transformationInMassiv(text);
         String[] wordsFromTheWord = transformationInMassiv(word);
         int count;
-        for (String d : wordsFromTheWord) {
+        for (String d : wordsFromTheWord) { // плохие имена для переменных f и d, такие наименование можно делать только в цилках типа for, особенно плохо читается d.compareToIgnoreCase(f)
             count=0;
             for (String f : wordsFromTheText) {
-                if (((d.compareToIgnoreCase(f))==0)&&(ignorCase==true))
+                if (((d.compareToIgnoreCase(f))==0)&&(ignorCase==true)) // можно ignoreCase==true заменить просто на ignoreCase
                     count++;
                 else { if((d.compareTo(f))==0) count++; }
             }
             countWords.put(d,count);
         }
-        return countWords;
+        return countWords; //в данном примере метод надо делать void, т.к. countWords итак статический, нет смысла его возвращать
     }
     
-    public static String[] transformationInMassiv(String text) {
+    public static String[] transformationInMassiv(String text) {//метод желательно сделать private, т.к. больше нигде не предполагается его использовать
         text = text.replaceAll(" ", "_");
         text = text.replaceAll("\\W", "_");
-        while (true) {            
-            if (text.indexOf("__")!=-1) {
+        while (true) {
+            if (text.indexOf("__")!=-1) { // лучше заменить на text.contains("__")
                 text = text.replaceAll("__", "_");
                 continue;
             } break;
         }
-        return text.split("_");
+        return text.split("_"); // слишком много replaceAll в методе, все можно заменить на return text.replaceAll("[^A-Za-z]+", " ").split(" ");
     }
 }
